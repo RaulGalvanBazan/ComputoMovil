63ef893a2b1202e4a5c9bbb4bbf2b942c82ca37d
RaulGalvanBazan <42521723+RaulGalvanBazan@users.noreply.github.com>
2019-09-08 15:55:01 -0500

    Add files via upload

---
+++ calculadora/calculadora/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  calculadora
+//
+//  Created by 2020-1 on 9/4/19.
+//  Copyright © 2019 unam. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+  
+    @IBAction func butAC(_ sender: Any) {
+    }
+    
+}
+
---
+++ calculadora/calculadora/calculator.swift
@@ -0,0 +1,9 @@
+//
+//  calculator.swift
+//  calculadora
+//
+//  Created by 2020-1 on 9/4/19.
+//  Copyright © 2019 unam. All rights reserved.
+//
+
+import Foundation
